b609bc043502b7b719e4582afb05ba5b04fe7dc2
andrewhong2022 <andrewhong2022@u.northwestern.edu>
2021-10-09 00:03:00 -0500

    read database

---
+++ src/utilities/firebase.js
@@ -0,0 +1,42 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { useState, useEffect } from 'react';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBHG8zxcIkgUDxW9LbsKIbezUaKjBnHVck",
+    authDomain: "scheduler-497.firebaseapp.com",
+    databaseURL: "https://scheduler-497-default-rtdb.firebaseio.com",
+    projectId: "scheduler-497",
+    storageBucket: "scheduler-497.appspot.com",
+    messagingSenderId: "174225741347",
+    appId: "1:174225741347:web:877209b8d8857a2751c396",
+    measurementId: "G-GT1T9LYB0W"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+};
